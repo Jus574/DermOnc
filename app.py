97f531e1b342417a849d5d6966bf15d5c171f642
Jus574 <juswinchackothomas123@gmail.com>
2024-02-27 19:36:37 +0530

    style changes

--- app.py
+++ app.py
@@ -8,29 +8,42 @@ from tensorflow.keras.models import model_from_json
 
 app = Flask(__name__)
 
-# Load the JSON file that contains the model architecture
-with open("kkmodel.json", "r") as json_file:
+# Load the benign vs malignant model
+with open("static/model/best_model.json", "r") as json_file:
     loaded_model_json = json_file.read()
-
-# Load the model architecture from the JSON
 loaded_model_imageNet = model_from_json(loaded_model_json)
+loaded_model_imageNet.load_weights("static/model/best_model.hdf5")
 
-# Load the model weights
-loaded_model_imageNet.load_weights("DenseNet201_ph1_weights.hdf5")
+# Load the Melanoma vs Basal Cell Carcinoma model
+with open("static/model/malignant_model.json", "r") as json_file:
+    malignant_model_json = json_file.read()
+malignant_model_imageNet = model_from_json(malignant_model_json)
+malignant_model_imageNet.load_weights("static/model/malignant_model.hdf5")
 
 
-def secondcheck(filepath):
-    img = cv2.imread(filepath)
-    x = np.expand_dims(img, axis=0)
-    x1 = preprocess_input(x)
-    result1 = loaded_model_imageNet.predict(x1)
-    ress = list((result1 * 100).astype("int"))
-    return ress
+def secondcheck(img):
+    xw = np.expand_dims(img, axis=0)
+    x1w = preprocess_input(xw)
+    result2 = malignant_model_imageNet.predict(x1w)
+    ressuu2 = list((result2 * 100).astype("int"))
+    ress2 = list(ressuu2[0])
+
+    if ress2[0] > ress2[1]:
+        disea = 3
+    else:
+        disea = 4
+
+    return ress2, disea
 
 
 def process_image(img_path):
     img = cv2.imread(img_path)
     img = cv2.resize(img, (128, 128))
+    grayScale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (17, 17))
+    blackhat = cv2.morphologyEx(grayScale, cv2.MORPH_BLACKHAT, kernel)
+    ret, threshold = cv2.threshold(blackhat, 0, 255, cv2.THRESH_BINARY)
+    final_image = cv2.inpaint(img, threshold, 1, cv2.INPAINT_TELEA)
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
     _, thresholded_image = cv2.threshold(
@@ -64,12 +77,13 @@ def upload_file():
         f.save(filepath)
         result, img = process_image(filepath)
         p = list((result * 100).astype("int"))
-        pp = list(p[0])
+        ressu = list(p[0])
+        dis = 1
 
-        # if pp[1] < pp[0]:
-        #     ressu = secondcheck(filepath)
+        if ressu[1] > ressu[0]:
+            ressu, dis = secondcheck(img)
 
-    return render_template("result.html", result=pp, img_path=file_name)
+    return render_template("result.html", result=ressu, img_path=file_name, disease=dis)
 
 
 if __name__ == "__main__":
